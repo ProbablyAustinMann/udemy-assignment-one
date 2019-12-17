09e80a8b64d2a171f0fb18e182b34d67c9a508eb
Austin Taylor Mann <Fancysause@gmail.com>
2019-12-16 19:43:40 -0500

    removed unnecessary button, changed variable names

--- src/App.js
+++ src/App.js
@@ -5,42 +5,28 @@ import User from './User/User';
 class App extends Component {
 
   state = {
-  persons: [
-    { name: 'Austin', age: 26 },
-    { name: 'Rodney', age: 7 },
-    { name: 'Nolan', age: 23 }
+  users: [
+    { name: 'Austin', comment: 'Hello!' }
   ]
 };
 
   inputHandler = (event) => {
     this.setState({
-      persons: [
-        { name: 'Austin', age: 26 },
-        { name: event.target.value, age: 7 },
-        { name: 'Nolan', age: 23 }
+      users: [
+        { name: event.target.value, comment: 'State Changed' }
       ]
     });
   };
 
   render() {
-      const style = {
-        backgroundColor: 'gray',
-        font: 'inherit',
-        border: '4px solid orange',
-        padding: '8px',
-        cursor: 'pointer'
-      };
 
     return (
       <div className="App">
       <h1>User Input</h1>
       <p>Feel free to type some things!</p>
-      <button
-      style={style}
-      onClick={() => this.inputHandler('Aus?')}>Switch Name</button>
       <User
-        name={this.state.persons[1].name}
-        age={this.state.persons[1].age}
+        name={this.state.users[0].name}
+        comment={this.state.users[0].comment}
         click={this.inputHandler.bind(this, 'Aus!!')}
         changed={this.inputHandler}>My Hobbies: Sleeping</User>
       </div>
--- src/User/User.js
+++ src/User/User.js
@@ -4,8 +4,8 @@ import './User.css';
 const user = (props) => {
   return (
     <div className='User'>
-        <p onClick={props.click}>I am {props.name} and I am {props.age} years old!</p>
-        <p>{props.children}</p>
+        <p onClick={props.click}>{props.name}</p>
+        <p>{props.comment}</p>
         <input type='text' onChange={props.changed} value={props.name}/>
     </div>
   )
